3dc9354698c063e0375ffb3baab121239049c8f5
Christian <c.algordo@yahoo.com>
2023-08-31 06:22:26 +0800

    update

--- Groups/Qilin_Blog.py
+++ Groups/Qilin_Blog.py
@@ -1,27 +1,35 @@
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 import pandas as pd
 import time
 
-# set up TOR and the TOR browser
-tor_proxy = "socks5://127.0.0.1:9150"
-options = webdriver.FirefoxOptions()
-options.set_preference('network.proxy.type', 1)
-options.set_preference('network.proxy.socks', '127.0.0.1')
-options.set_preference('network.proxy.socks_port', 9150)
-options.set_preference('network.proxy.socks_remote_dns', True)
+try:
 
-# Set the WebDriver to run in headless mode
-options.headless = True
+    # set up TOR and the TOR browser
+    tor_proxy = "socks5://127.0.0.1:9150"
+    options = webdriver.FirefoxOptions()
+    options.set_preference('network.proxy.type', 1)
+    options.set_preference('network.proxy.socks', '127.0.0.1')
+    options.set_preference('network.proxy.socks_port', 9150)
+    options.set_preference('network.proxy.socks_remote_dns', True)
 
-# Create a Firefox WebDriver instance with the options
-driver = webdriver.Firefox(options=options)
+    # Set the WebDriver to run in headless mode
+    options.headless = True
 
-# navigate to the website
-site = 'http://kbsqoivihgdmwczmxkbovk7ss2dcynitwhhfu5yw725dboqo5kthfaad.onion/'
-driver.get(site)
-print("Success!, Success!, Success! , Success!, Success!, Success!, Success!, Success!, Success!, Success!, Success!, Success!")
+    # Create a Firefox WebDriver instance with the options
+    driver = webdriver.Firefox(options=options)
 
-# You can perform additional actions here
+    # navigate to the website
+    site = 'http://kbsqoivihgdmwczmxkbovk7ss2dcynitwhhfu5yw725dboqo5kthfaad.onion/'
+    driver.get(site)
+    print("Success!, Success!, Success! , Success!, Success!, Success!, Success!, Success!, Success!, Success!, Success!, Success!")
 
-# Close the driver when done
-driver.quit()
+    # You can perform additional actions here
+except WebDriverException as e:
+    print("WebDriver error:", e)
+
+finally:
+    # Make sure to quit the WebDriver regardless of success or failure
+    if 'driver' in locals():
+        driver.quit()
+
