5ecbbffed17b78231a3e78253411f45a3ae91624
Christian <c.algordo@yahoo.com>
2023-09-01 07:21:28 +0800

    update

--- Groups/Qilin_Blog.py
+++ Groups/Qilin_Blog.py
@@ -2,20 +2,21 @@ from selenium import webdriver
 
 # Tor SOCKS proxy configuration
 tor_proxy = "socks5://127.0.0.1:9150"
-proxy_options = {
-    'proxy': {
-        'socksProxy': tor_proxy,
-        'socksVersion': 5,
-    }
-}
 
 # Configure Firefox options
 firefox_options = webdriver.FirefoxOptions()
 firefox_options.add_argument('--headless')  # Run Firefox in headless mode
 firefox_options.add_argument('--no-sandbox')  # Required for running in a container
 
-# Configure the WebDriver with proxy and options
-driver = webdriver.Firefox(options=firefox_options, proxy=proxy_options)
+# Set up proxy preferences
+firefox_profile = webdriver.FirefoxProfile()
+firefox_profile.set_preference('network.proxy.type', 1)
+firefox_profile.set_preference('network.proxy.socks', '127.0.0.1')
+firefox_profile.set_preference('network.proxy.socks_port', 9150)
+firefox_profile.set_preference('network.proxy.socks_remote_dns', True)
+
+# Configure the WebDriver with options and profile
+driver = webdriver.Firefox(firefox_profile=firefox_profile, options=firefox_options)
 
 # Navigate to the website
 site = 'http://kbsqoivihgdmwczmxkbovk7ss2dcynitwhhfu5yw725dboqo5kthfaad.onion/'
