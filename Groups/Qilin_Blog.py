2491fd68805c9c63e8707a37aa8f775416f51ff6
Christian <c.algordo@yahoo.com>
2023-09-01 07:17:24 +0800

    update

--- Groups/Qilin_Blog.py
+++ Groups/Qilin_Blog.py
@@ -1,20 +1,21 @@
 from selenium import webdriver
 
-# Set up TOR and the TOR browser
+# Tor SOCKS proxy configuration
 tor_proxy = "socks5://127.0.0.1:9150"
-options = webdriver.FirefoxOptions()
-options.add_argument('--headless')
-options.set_preference('network.proxy.type', 1)
-options.set_preference('network.proxy.socks', '127.0.0.1')
-options.set_preference('network.proxy.socks_port', 9150)
-options.set_preference('network.proxy.socks_remote_dns', True)
+proxy_options = {
+    'proxy': {
+        'socksProxy': tor_proxy,
+        'socksVersion': 5,
+    }
+}
 
-# Set the PATH environment variable to include the directory where geckodriver is located
-import os
-os.environ['PATH'] = f'{os.getcwd()}{os.pathsep}{os.environ["PATH"]}'
+# Configure Firefox options
+firefox_options = webdriver.FirefoxOptions()
+firefox_options.add_argument('--headless')  # Run Firefox in headless mode
+firefox_options.add_argument('--no-sandbox')  # Required for running in a container
 
-# Create a Firefox WebDriver instance
-driver = webdriver.Firefox(options=options)
+# Configure the WebDriver with proxy and options
+driver = webdriver.Firefox(options=firefox_options, proxy=proxy_options)
 
 # Navigate to the website
 site = 'http://kbsqoivihgdmwczmxkbovk7ss2dcynitwhhfu5yw725dboqo5kthfaad.onion/'
@@ -22,3 +23,6 @@ driver.get(site)
 
 # Perform additional actions here
 print("Success, Success, Success")
+
+# Close the WebDriver
+driver.quit()
--- Groups/sample.py
+++ Groups/sample.py
@@ -25,3 +25,28 @@ print("Success!, Success!, Success! , Success!, Success!, Success!, Success!, Su
 
 # Close the driver when done
 driver.quit()
+
+from selenium import webdriver
+
+# Set up TOR and the TOR browser
+tor_proxy = "socks5://127.0.0.1:9150"
+options = webdriver.FirefoxOptions()
+options.add_argument('--headless')
+options.set_preference('network.proxy.type', 1)
+options.set_preference('network.proxy.socks', '127.0.0.1')
+options.set_preference('network.proxy.socks_port', 9150)
+options.set_preference('network.proxy.socks_remote_dns', True)
+
+# Set the PATH environment variable to include the directory where geckodriver is located
+import os
+os.environ['PATH'] = f'{os.getcwd()}{os.pathsep}{os.environ["PATH"]}'
+
+# Create a Firefox WebDriver instance
+driver = webdriver.Firefox(options=options)
+
+# Navigate to the website
+site = 'http://kbsqoivihgdmwczmxkbovk7ss2dcynitwhhfu5yw725dboqo5kthfaad.onion/'
+driver.get(site)
+
+# Perform additional actions here
+print("Success, Success, Success")
