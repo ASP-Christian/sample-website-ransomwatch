1c737fbbdcb96034ad708b91948c2efda754f21b
Christian <c.algordo@yahoo.com>
2023-08-31 09:37:49 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,14 @@
-FROM ubuntu:latest
-
-# Install Tor and other dependencies
-RUN apt-get update && apt-get install -y tor firefox
+# Use a base image that includes TOR and Python
+FROM some-tor-python-image
 
 # Copy your script to the container
 COPY Groups/Qilin_Blog.py /app/Qilin_Blog.py
 
-CMD ["bash"]
+# Set the working directory
+WORKDIR /app
+
+# Set up any additional dependencies
+RUN pip install pandas selenium
+
+# Define the command to run your script
+CMD ["python", "Qilin_Blog.py"]
--- Groups/Qilin_Blog.py
+++ Groups/Qilin_Blog.py
@@ -1,34 +1,23 @@
-import os
 from selenium import webdriver
 import pandas as pd
 import time
+import os
 
-# Read proxy details from environment variables
-tor_proxy = f"socks5://{os.environ['ONION_PROXY_URL']}:{os.environ['ONION_PROXY_PORT'].strip()}"
-tor_username = os.environ['ONION_PROXY_USERNAME']
-tor_password = os.environ['ONION_PROXY_PASSWORD']
+tor_proxy = os.getenv('TOR_PROXY', "socks5://127.0.0.1:9150")
 
 options = webdriver.FirefoxOptions()
 options.set_preference('network.proxy.type', 1)
-options.set_preference('network.proxy.socks', tor_proxy)
-options.set_preference('network.proxy.socks_port', int(os.environ['ONION_PROXY_PORT'].strip()))
+options.set_preference('network.proxy.socks', '127.0.0.1')
+options.set_preference('network.proxy.socks_port', 9150)
 options.set_preference('network.proxy.socks_remote_dns', True)
 options.headless = True
 
-# Set Tor proxy authentication
-profile = webdriver.FirefoxProfile()
-profile.set_preference("network.proxy.socks_username", tor_username)
-profile.set_preference("network.proxy.socks_password", tor_password)
+driver = webdriver.Firefox(options=options)
 
-# Create a Firefox WebDriver instance with the options
-driver = webdriver.Firefox(options=options, firefox_profile=profile)
-
-# navigate to the website
 site = 'http://kbsqoivihgdmwczmxkbovk7ss2dcynitwhhfu5yw725dboqo5kthfaad.onion/'
 driver.get(site)
-print("Success! Connected via Tor.")
+print("Success! The script ran successfully!")
 
-# You can perform additional actions here
+# Additional actions you want to perform here
 
-# Close the driver when done
 driver.quit()
