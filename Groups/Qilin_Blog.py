d10ffe9bdda1a995be1d7d4875320561f350e2b1
Christian <c.algordo@yahoo.com>
2023-09-01 04:04:44 +0800

    update

--- Groups/Qilin_Blog.py
+++ Groups/Qilin_Blog.py
@@ -1,30 +1,23 @@
 from selenium import webdriver
 
-import time
-
-# set up TOR and the TOR browser
+# Set up TOR and the TOR browser
 tor_proxy = "socks5://127.0.0.1:9150"
 options = webdriver.FirefoxOptions()
-options.add_argument('-headless')
+options.add_argument('--headless')
 options.set_preference('network.proxy.type', 1)
 options.set_preference('network.proxy.socks', '127.0.0.1')
 options.set_preference('network.proxy.socks_port', 9150)
 options.set_preference('network.proxy.socks_remote_dns', True)
-driver = webdriver.Firefox(options=options)
 
+# Set the WebDriver executable path
+driver_path = './geckodriver'  # Update the path here
+driver = webdriver.Firefox(executable_path=driver_path, options=options)
 
-# Set the WebDriver to run in headless mode
-options.headless = True
-
-# Create a Firefox WebDriver instance with the options
-driver = webdriver.Firefox(options=options)
-
-# navigate to the website
+# Navigate to the website
 site = 'http://kbsqoivihgdmwczmxkbovk7ss2dcynitwhhfu5yw725dboqo5kthfaad.onion/'
 driver.get(site)
-print("Success!, Success!, Success! , Success!, Success!, Success!, Success!, Success!, Success!, Success!, Success!, Success!")
 
-# You can perform additional actions here
-
+# Perform additional actions here
+print("Success, Success, Success")
 # Close the driver when done
 driver.quit()
