bcc87b5aebe8b4fc31ddc333d35f8750b26fccc1
Christian <c.algordo@yahoo.com>
2023-09-01 07:24:44 +0800

    update

--- Groups/Qilin_Blog.py
+++ Groups/Qilin_Blog.py
@@ -9,14 +9,13 @@ firefox_options.add_argument('--headless')  # Run Firefox in headless mode
 firefox_options.add_argument('--no-sandbox')  # Required for running in a container
 
 # Set up proxy preferences
-firefox_profile = webdriver.FirefoxProfile()
-firefox_profile.set_preference('network.proxy.type', 1)
-firefox_profile.set_preference('network.proxy.socks', '127.0.0.1')
-firefox_profile.set_preference('network.proxy.socks_port', 9150)
-firefox_profile.set_preference('network.proxy.socks_remote_dns', True)
+firefox_options.set_preference('network.proxy.type', 1)
+firefox_options.set_preference('network.proxy.socks', '127.0.0.1')
+firefox_options.set_preference('network.proxy.socks_port', 9150)
+firefox_options.set_preference('network.proxy.socks_remote_dns', True)
 
-# Configure the WebDriver with options and profile
-driver = webdriver.Firefox(firefox_profile=firefox_profile, options=firefox_options)
+# Configure the WebDriver with options
+driver = webdriver.Firefox(options=firefox_options)
 
 # Navigate to the website
 site = 'http://kbsqoivihgdmwczmxkbovk7ss2dcynitwhhfu5yw725dboqo5kthfaad.onion/'
