9f4a0f5c978eff69246a3e04a412cda3e5b03632
Christian <c.algordo@yahoo.com>
2023-09-08 04:37:36 +0800

    update

--- your_selenium_script.py
+++ your_selenium_script.py
@@ -1,14 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.proxy import Proxy, ProxyType
 
-tor_proxy = "socks5://127.0.0.1:9150"  # Tor proxy address
-
+tor_proxy = "socks5://127.0.0.1:9050"
 profile = webdriver.FirefoxProfile()
 profile.set_preference("network.proxy.type", 1)
 profile.set_preference("network.proxy.socks", "127.0.0.1")
-profile.set_preference("network.proxy.socks_port", 9150)
+profile.set_preference("network.proxy.socks_port", 9050)
+profile.set_preference("network.proxy.socks_version", 5)
 
 driver = webdriver.Firefox(firefox_profile=profile)
-driver.get('https://3f7nxkjway3d223j27lyad7v5cgmyaifesycvmwq7i7cbs23lb6llryd.onion/')
-# Your scraping code here
-print('Armlet Gibaho')
-driver.quit()
