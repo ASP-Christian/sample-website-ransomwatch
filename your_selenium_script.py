a230c4b1ffa3d341a51493425550c230c49019fe
Christian <c.algordo@yahoo.com>
2023-09-08 05:00:06 +0800

    update

--- your_selenium_script.py
+++ your_selenium_script.py
@@ -4,5 +4,6 @@ from selenium.webdriver.firefox.options import Options
 options = Options()
 options.add_argument('--proxy-server=socks5://127.0.0.1:9050')
 options.add_argument('--log-level=DEBUG')  # Add this line to enable WebDriver logging
+options.add_argument('--headless')
 
 driver = webdriver.Firefox(options=options)
