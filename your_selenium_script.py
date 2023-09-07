6ec2689bb2429dc617e44674e1148f1070e1ee16
Christian <c.algordo@yahoo.com>
2023-09-08 04:57:45 +0800

    update

--- your_selenium_script.py
+++ your_selenium_script.py
@@ -1,9 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.common.proxy import Proxy, ProxyType
 from selenium.webdriver.firefox.options import Options
 
 options = Options()
 options.add_argument('--proxy-server=socks5://127.0.0.1:9050')
+options.add_argument('--log-level=DEBUG')  # Add this line to enable WebDriver logging
 
 driver = webdriver.Firefox(options=options)
-
