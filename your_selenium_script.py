81c6b823388a15113414108f2fa95f4289915a3a
Christian <c.algordo@yahoo.com>
2023-09-08 06:03:09 +0800

    update

--- your_selenium_script.py
+++ your_selenium_script.py
@@ -6,8 +6,17 @@ options.add_argument('--proxy-server=socks5://127.0.0.1:9050')
 options.add_argument('--log-level=DEBUG')  # Add this line to enable WebDriver logging
 options.add_argument('--headless')
 
+# Adjust the URL to the onion site you want to visit
+onion_url = 'https://3f7nxkjway3d223j27lyad7v5cgmyaifesycvmwq7i7cbs23lb6llryd.onion/'
+
 driver = webdriver.Firefox(options=options)
 
+# Visit the onion site
+driver.get(onion_url)
 
+# Perform actions on the site if needed
+# For example, you can extract information or interact with elements
+
+# Close the browser when done
 driver.quit()
 print('Armlet Armlet')
