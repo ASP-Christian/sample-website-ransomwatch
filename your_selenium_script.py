c822710b76d031a54304ac74621dce04993b49b2
Christian <c.algordo@yahoo.com>
2023-09-08 08:15:22 +0800

    update

--- your_selenium_script.py
+++ your_selenium_script.py
@@ -10,21 +10,12 @@ options.add_argument('--headless')
 # Adjust the URL to the onion site you want to visit
 onion_url = 'https://3f7nxkjway3d223j27lyad7v5cgmyaifesycvmwq7i7cbs23lb6llryd.onion/'
 
-try:
-    driver = webdriver.Firefox(options=options)
 
-    # Visit the onion site
-    driver.get(onion_url)
+driver = webdriver.Firefox(options=options)
 
-    # Get and print the title of the site
-    site_title = driver.title
-    print(f'Title of the site: {site_title}')
+# Visit the onion site
+driver.get(onion_url)
 
-except WebDriverException as e:
-    print(f'Selenium WebDriver Error: {str(e)}')
-except Exception as e:
-    print(f'An error occurred: {str(e)}')
-finally:
-    # Close the browser when done or if an error occurs
-    if 'driver' in locals():
-        driver.quit()
+# Get and print the title of the site
+site_title = driver.title
+print(f'Title of the site: {site_title}')
