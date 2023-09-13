661b4761194378450a200c561b0905f3379a86cc
Christian <c.algordo@yahoo.com>
2023-09-13 10:54:38 +0800

    update

--- Groups/sample.py
+++ Groups/sample.py
@@ -1,29 +1,23 @@
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 
-try:
-    tor_proxy = "socks5://127.0.0.1:9150"
-    options = webdriver.FirefoxOptions()
-    options.set_preference('network.proxy.type', 1)
-    options.set_preference('network.proxy.socks', '127.0.0.1')
-    options.set_preference('network.proxy.socks_port', 9150)
-    options.set_preference('network.proxy.socks_remote_dns', True)
 
-    options.add_argument('-headless')
+tor_proxy = "socks5://127.0.0.1:9150"
+options = webdriver.FirefoxOptions()
+options.set_preference('network.proxy.type', 1)
+options.set_preference('network.proxy.socks', '127.0.0.1')
+options.set_preference('network.proxy.socks_port', 9150)
+options.set_preference('network.proxy.socks_remote_dns', True)
 
-    driver = webdriver.Firefox(options=options)
+options.add_argument('-headless')
 
-    site = 'https://3f7nxkjway3d223j27lyad7v5cgmyaifesycvmwq7i7cbs23lb6llryd.onion/'
-    driver.get(site)
+driver = webdriver.Firefox(options=options)
 
-    # Get and print the title of the website
-    print("Title:", driver.title)
+site = 'https://3f7nxkjway3d223j27lyad7v5cgmyaifesycvmwq7i7cbs23lb6llryd.onion/'
+driver.get(site)
 
-except WebDriverException as e:
-    print("WebDriverException:", str(e))
-except Exception as e:
-    print("An unexpected error occurred:", str(e))
+# Get and print the title of the website
+print("Title:", driver.title)
 
-finally:
-    # Close the driver
-    driver.quit()
+
+driver.quit()
