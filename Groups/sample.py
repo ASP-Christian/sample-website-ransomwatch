77d188de0e6df631cb2860433c104b8b0133125e
Christian <c.algordo@yahoo.com>
2023-09-13 08:33:59 +0800

    update

--- Groups/sample.py
+++ Groups/sample.py
@@ -1,16 +1,15 @@
 from selenium import webdriver
 
-# Set up the Firefox WebDriver with geckodriver (no need to specify path)
-driver = webdriver.Firefox()
+# Set up the WebDriver (you may need to adjust the path to geckodriver)
+driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
 
 # Navigate to the website
-url = "https://chrisodrogla.github.io/My-Website-Portfolio/"
-driver.get(url)
+driver.get("https://chrisodrogla.github.io/My-Website-Portfolio/")
 
 # Get the title of the website
 title = driver.title
 
-# Print the title to the console
+# Print the title to the console (you can modify this part as needed)
 print("Website Title:", title)
 
 # Close the WebDriver
