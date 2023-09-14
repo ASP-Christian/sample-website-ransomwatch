7157c6ad66a2c8f206997e64d6e1ff15f28d255e
Christian <c.algordo@yahoo.com>
2023-09-14 08:50:48 +0800

    update

--- Groups/sample.py
+++ Groups/sample.py
@@ -1,14 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.proxy import Proxy, ProxyType
 
 # Set up Firefox options for headless mode
 options = Options()
 options.headless = True
 
-# Initialize the Firefox WebDriver with the specified options
-driver = webdriver.Firefox(options=options)
+# Set up the Tor SOCKS proxy
+tor_proxy_port = 9150  # Replace with the actual SOCKS proxy port if it's different
+proxy = Proxy()
+proxy.proxy_type = ProxyType.MANUAL
+proxy.http_proxy = f"socks5://127.0.0.1:{tor_proxy_port}"
+proxy.socks_proxy = f"socks5://127.0.0.1:{tor_proxy_port}"
+proxy.ssl_proxy = f"socks5://127.0.0.1:{tor_proxy_port}"
 
-# Replace 'https://example.com' with the URL of the website you want to extract the title from
+# Initialize the Firefox WebDriver with the specified options and proxy settings
+driver = webdriver.Firefox(options=options, proxy=proxy)
+
+# Replace 'https://example.com' with the URL of the website you want to visit
 url = 'https://chrisodrogla.github.io/My-Website-Portfolio/'
 driver.get(url)
 
