5a241c48b345784ec2054bf12e73f92a6afb9940
Christian <c.algordo@yahoo.com>
2023-09-14 08:54:15 +0800

    update

--- Groups/sample.py
+++ Groups/sample.py
@@ -1,24 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.proxy import Proxy, ProxyType
 
 # Set up Firefox options for headless mode
 options = Options()
 options.headless = True
 
-# Set up the Tor SOCKS proxy
-tor_proxy_port = 9150  # Replace with the actual SOCKS proxy port if it's different
-proxy = Proxy()
-proxy.proxy_type = ProxyType.MANUAL
-proxy.http_proxy = f"socks5://127.0.0.1:{tor_proxy_port}"
-proxy.socks_proxy = f"socks5://127.0.0.1:{tor_proxy_port}"
-proxy.ssl_proxy = f"socks5://127.0.0.1:{tor_proxy_port}"
+# Create a Firefox profile with the proxy settings
+firefox_profile = webdriver.FirefoxProfile()
+firefox_profile.set_preference("network.proxy.type", 1)  # Use manual proxy configuration
+firefox_profile.set_preference("network.proxy.socks", "127.0.0.1")  # Replace with the proxy IP address
+firefox_profile.set_preference("network.proxy.socks_port", 9150)  # Replace with the Tor SOCKS proxy port
 
-# Initialize the Firefox WebDriver with the specified options and proxy settings
-driver = webdriver.Firefox(options=options, proxy=proxy)
+# Initialize the Firefox WebDriver with the specified options and profile
+driver = webdriver.Firefox(firefox_profile=firefox_profile, options=options)
 
 # Replace 'https://example.com' with the URL of the website you want to visit
 url = 'https://chrisodrogla.github.io/My-Website-Portfolio/'
+
 driver.get(url)
 
 # Extract and print the title of the webpage
@@ -27,3 +25,5 @@ print(f'Title of the webpage: {title}')
 
 # Close the browser
 driver.quit()
+
+
