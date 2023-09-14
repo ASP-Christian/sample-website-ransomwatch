048931cc343a586cf00bcf8767d39aa8cb390415
Christian <c.algordo@yahoo.com>
2023-09-14 08:16:21 +0800

    update

--- Groups/sample.py
+++ Groups/sample.py
@@ -1,39 +1,20 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-from stem import Signal
-from stem.control import Controller
-import time
+from selenium.webdriver.firefox.options import Options
 
-# Function to change Tor IP address
-def change_tor_ip():
-    with Controller.from_port(port=9051) as controller:
-        controller.authenticate(password="your_password")  # Replace with your Tor password
-        controller.signal(Signal.NEWNYM)
+# Set up Firefox options for headless mode
+options = Options()
+options.headless = True
 
-# Set up Firefox with Tor proxy
-binary = FirefoxBinary()  # No need to specify the binary path
-profile = webdriver.FirefoxProfile()
-profile.set_preference("network.proxy.type", 1)
-profile.set_preference("network.proxy.socks", "127.0.0.1")
-profile.set_preference("network.proxy.socks_port", 9050)
+# Initialize the Firefox WebDriver with the specified options
+driver = webdriver.Firefox(options=options)
 
-
-# Create a Firefox WebDriver instance
-driver = webdriver.Firefox(firefox_binary=binary, firefox_profile=profile)
-
-# Navigate to the website
-url = "https://3f7nxkjway3d223j27lyad7v5cgmyaifesycvmwq7i7cbs23lb6llryd.onion/"
+# Replace 'https://example.com' with the URL of the website you want to extract the title from
+url = 'https://www.apps.disneyplus.com/ph/shows/family-guy/1260024995/better-off-meg/1260025867/watch?episodeNumber=17&filters=content_type%3Depisode&seasonId=71'
 driver.get(url)
 
-# Wait for the page to load (adjust the sleep time as needed)
-time.sleep(10)
-
-# Scrape the title
+# Extract and print the title of the webpage
 title = driver.title
-print("Title:", title)
+print(f'Title of the webpage: {title}')
 
-# Close the Firefox WebDriver
+# Close the browser
 driver.quit()
-
-# Change Tor IP address to avoid IP blocking
-change_tor_ip()
