677272df2ff0aa1836d410d30782f1407af58897
Christian <c.algordo@yahoo.com>
2023-09-14 08:55:30 +0800

    update

--- Groups/sample.py
+++ Groups/sample.py
@@ -1,22 +1,29 @@
 from selenium import webdriver
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.firefox.options import Options
 
 # Set up Firefox options for headless mode
 options = Options()
 options.headless = True
 
-# Create a Firefox profile with the proxy settings
-firefox_profile = webdriver.FirefoxProfile()
-firefox_profile.set_preference("network.proxy.type", 1)  # Use manual proxy configuration
-firefox_profile.set_preference("network.proxy.socks", "127.0.0.1")  # Replace with the proxy IP address
-firefox_profile.set_preference("network.proxy.socks_port", 9150)  # Replace with the Tor SOCKS proxy port
+# Set up the Tor SOCKS proxy
+tor_proxy_port = 9150  # Replace with the actual SOCKS proxy port if it's different
+proxy = Proxy()
+proxy.proxy_type = ProxyType.MANUAL
+proxy.http_proxy = f"socks5://127.0.0.1:{tor_proxy_port}"
+proxy.socks_proxy = f"socks5://127.0.0.1:{tor_proxy_port}"
+proxy.ssl_proxy = f"socks5://127.0.0.1:{tor_proxy_port}"
 
-# Initialize the Firefox WebDriver with the specified options and profile
-driver = webdriver.Firefox(firefox_profile=firefox_profile, options=options)
+# Create a DesiredCapabilities object and set the proxy
+capabilities = DesiredCapabilities.FIREFOX.copy()
+proxy.add_to_capabilities(capabilities)
+
+# Initialize the Firefox WebDriver with the specified options and capabilities
+driver = webdriver.Firefox(options=options, desired_capabilities=capabilities)
 
 # Replace 'https://example.com' with the URL of the website you want to visit
-url = 'https://chrisodrogla.github.io/My-Website-Portfolio/'
-
+url = 'https://example.com'
 driver.get(url)
 
 # Extract and print the title of the webpage
@@ -25,5 +32,3 @@ print(f'Title of the webpage: {title}')
 
 # Close the browser
 driver.quit()
-
-
