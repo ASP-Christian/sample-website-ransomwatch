23a76f42d52f1a21823e73af8a0dd44acaafa66f
Christian <c.algordo@yahoo.com>
2023-09-20 21:26:57 +0800

    update

--- Groups/sample2.py
+++ Groups/sample2.py
@@ -5,13 +5,24 @@ import time
 # Set up the TOR proxy
 tor_proxy = "127.0.0.1:9150"  # Use your Tor proxy address without the scheme
 
+# If proxy requires authentication, add your credentials here
+# tor_proxy_username = "your_username"
+# tor_proxy_password = "your_password"
+
 proxy = Proxy()
 proxy.proxy_type = ProxyType.MANUAL
 proxy.http_proxy = tor_proxy
 proxy.ssl_proxy = tor_proxy
 
+# Add proxy authentication if required
+# proxy.http_proxy = f"{tor_proxy_username}:{tor_proxy_password}@{tor_proxy}"
+# proxy.ssl_proxy = f"{tor_proxy_username}:{tor_proxy_password}@{tor_proxy}"
+
 capabilities = webdriver.DesiredCapabilities.FIREFOX
 proxy.add_to_capabilities(capabilities)
+
+# Wait for Tor proxy to fully start (adjust sleep duration as needed)
+time.sleep(5)
 
 # Set up headless mode for Firefox
 options = webdriver.FirefoxOptions()
