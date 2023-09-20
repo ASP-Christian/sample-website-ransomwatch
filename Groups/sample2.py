00446c9716210439e371f918c89a58f37d532368
Christian <c.algordo@yahoo.com>
2023-09-20 22:33:40 +0800

    update

--- Groups/sample2.py
+++ Groups/sample2.py
@@ -6,30 +6,33 @@ import requests
 # Set up the TOR proxy
 tor_proxy = "127.0.0.1:9150"  # Use your Tor proxy address without the scheme
 
-proxy = Proxy()
-proxy.proxy_type = ProxyType.MANUAL
-proxy.http_proxy = tor_proxy
-proxy.ssl_proxy = tor_proxy
-
-capabilities = webdriver.DesiredCapabilities.FIREFOX
-proxy.add_to_capabilities(capabilities)
-
-# Check if the Tor proxy is reachable
-try:
-    requests.get("http://check.torproject.org", proxies={"http": tor_proxy, "https": tor_proxy}, timeout=10)
-except requests.RequestException as e:
-    print("Failed to connect to the Tor proxy:", str(e))
-    exit(1)
+# Function to check if Tor proxy is reachable
+def is_tor_proxy_reachable():
+    try:
+        requests.get("http://check.torproject.org", proxies={"http": tor_proxy, "https": tor_proxy}, timeout=10)
+        return True
+    except requests.RequestException as e:
+        print("Failed to connect to the Tor proxy:", str(e))
+        return False
 
 # Wait for Tor proxy to fully start (adjust sleep duration as needed)
-time.sleep(5)
+max_attempts = 30
+delay = 5
+for _ in range(max_attempts):
+    if is_tor_proxy_reachable():
+        print("Tor proxy is reachable.")
+        break
+    time.sleep(delay)
+else:
+    print("Failed to reach Tor proxy after waiting.")
+    exit(1)
 
 # Set up headless mode for Firefox
 options = webdriver.FirefoxOptions()
 options.headless = True
 
 # Create a Firefox WebDriver instance with the options
-driver = webdriver.Firefox(capabilities=capabilities, options=options)
+driver = webdriver.Firefox(options=options)
 
 # Navigate to the website
 site = 'http://kbsqoivihgdmwczmxkbovk7ss2dcynitwhhfu5yw725dboqo5kthfaad.onion/'
