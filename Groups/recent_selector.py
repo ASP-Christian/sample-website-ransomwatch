a23c1ff19c38a77b90b7fb3f600a0bacb8667555
Christian <c.algordo@yahoo.com>
2023-09-22 12:02:39 +0800

    update

--- Groups/recent_selector.py
+++ Groups/recent_selector.py
@@ -10,10 +10,12 @@ end_of_week = start_of_week + datetime.timedelta(days=6)
 with open('Groups/Overall_data/data_post.json', 'r') as data_file:
     data = json.load(data_file)
 
-# Filter data within the current week
-recent_data = [entry for entry in data if start_of_week <= datetime.datetime.strptime(entry['data_date'], "%Y-%m-%d %H:%M:%S %Z%z").date() <= end_of_week]
+# Convert date strings to datetime objects for comparison
+data = [
+    entry for entry in data if
+    start_of_week <= datetime.datetime.strptime(entry['data_date'], "%Y-%m-%d").date() <= end_of_week
+]
 
 # Save the filtered data to recent_data.json
 with open('Groups/Overall_data/recent_data.json', 'w') as recent_data_file:
-    json.dump(recent_data, recent_data_file, indent=4)
-
+    json.dump(data, recent_data_file, indent=4)
