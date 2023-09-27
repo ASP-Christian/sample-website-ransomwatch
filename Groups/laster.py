a50aee5c080421de8e57bf263fbd3a301652dbf0
Christian <c.algordo@yahoo.com>
2023-09-27 17:33:33 +0800

    update

--- Groups/laster.py
+++ Groups/laster.py
@@ -4,11 +4,13 @@ from stem import Signal
 from stem.control import Controller
 from bs4 import BeautifulSoup
 
+
 # Function to renew the TOR IP address
 def renew_tor_ip():
     with Controller.from_port(port=9051) as controller:
         controller.authenticate(password="YOUR_TOR_PASSWORD")
         controller.signal(Signal.NEWNYM)
+
 
 # TOR proxy settings
 tor_proxy = {
@@ -23,14 +25,26 @@ url = 'https://3f7nxkjway3d223j27lyad7v5cgmyaifesycvmwq7i7cbs23lb6llryd.onion/'
 try:
     renew_tor_ip()  # Renew TOR IP address before making the request
     response = requests.get(url, proxies=tor_proxy, verify=False)
-    response.raise_for_status()
 
-    # Parse the HTML content
-    soup = BeautifulSoup(response.text, 'html.parser')
+    # Get the status code
+    status_code = response.status_code
 
-    # Get the title of the website
-    title = soup.title.string.strip()
-    print("Website Title:", title)
+    if status_code >= 200 and status_code < 300:
+        status_message = "Success"
+        is_active = True
+    elif status_code >= 400 and status_code < 500:
+        status_message = "Client Error"
+        is_active = False
+    elif status_code >= 500 and status_code < 600:
+        status_message = "Server Error"
+        is_active = False
+    else:
+        status_message = "Unknown"
+        is_active = False
+
+    print("Status Code:", status_code)
+    print("Status Message:", status_message)
+    print("Website Active:", is_active)
 
 except Exception as e:
     print("Error:", str(e))
