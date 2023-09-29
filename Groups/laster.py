34d5064342c9ecf98be4839aeeabf94ecd91ea51
Christian <c.algordo@yahoo.com>
2023-09-29 11:37:19 +0800

    update

--- Groups/laster.py
+++ Groups/laster.py
@@ -1,43 +1,51 @@
+import os
 import requests
-import socks
-import socket
-from bs4 import BeautifulSoup
 from stem import Signal
 from stem.control import Controller
+from bs4 import BeautifulSoup
 
-# Configure Tor proxy settings
-socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050)
-socket.socket = socks.socksocket
-
-
-# Function to renew Tor circuit
-def renew_tor_circuit():
+# Function to renew the TOR IP address
+def renew_tor_ip():
     with Controller.from_port(port=9051) as controller:
         controller.authenticate(password="YOUR_TOR_PASSWORD")
         controller.signal(Signal.NEWNYM)
 
+# TOR proxy settings
+tor_proxy = {
+    'http': 'socks5h://localhost:9050',
+    'https': 'socks5h://localhost:9050',
+}
 
-# URL of the website you want to scrape
-url = "http://3f7nxkjway3d223j27lyad7v5cgmyaifesycvmwq7i7cbs23lb6llryd.onion"
+# URL of the onion website
+url = 'https://3f7nxkjway3d223j27lyad7v5cgmyaifesycvmwq7i7cbs23lb6llryd.onion/'
 
-# Renew the Tor circuit before making a request
-renew_tor_circuit()
-
+# Send a request through the TOR proxy with certificate verification disabled
 try:
-    # Send a GET request through Tor
-    response = requests.get(url)
+    renew_tor_ip()  # Renew TOR IP address before making the request
+    response = requests.get(url, proxies=tor_proxy, verify=False)
+    response.raise_for_status()
 
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Parse the HTML content
-        soup = BeautifulSoup(response.text, "html.parser")
+    # Get the status code
+    status_code = response.status_code
 
-        # Extract and print the title
-        title = soup.title.string
-        print(f"Title: {title}")
-
+    # Determine if the website is active based on status code
+    if 200 <= status_code < 300:
+        is_active = True
     else:
-        print(f"Failed to fetch the webpage. Status code: {response.status_code}")
+        is_active = False
 
+    # Parse the HTML content
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Get the title of the website
+    title = soup.title.string.strip()
+    print("Website Title:", title)
+
+    # Print the status code and whether the website is active
+    print("Status Code:", status_code)
+    print("Is Active:", is_active)
+
+except requests.exceptions.RequestException as e:
+    print("Error:", e)
 except Exception as e:
-    print(f"An error occurred: {str(e)}")
+    print("An unexpected error occurred:", str(e))
