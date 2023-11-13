af164aa8a6a8c03c14d28e7e5785e27f49668477
Christian <c.algordo@yahoo.com>
2023-11-13 19:44:45 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Groups/Overall_data/data_post.json

--- Groups/sample.py
+++ Groups/sample.py
@@ -44,56 +44,36 @@ def is_internal_link(base_url, link):
     link_domain = urlparse(link).netloc
     return base_domain == link_domain
 
-def count_unique_group_urls(json_data):
-    unique_group_urls = set(item['group_url'] for item in json_data)
-    return len(unique_group_urls)
+def load_json(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
 
-def save_to_crawled_json(crawled_data):
-    with open('Groups/Overall_data/crawled.json', 'w', encoding='utf-8') as json_file:
-        json.dump(crawled_data, json_file, indent=2)
+def save_json(file_path, data):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=2)
 
 if __name__ == "__main__":
-    # Read starting URLs from data_post.json
-    with open('Groups/Overall_data/data1_post.json', 'r', encoding='utf-8') as json_file:
-        json_data = json.load(json_file)
-
-    # Count unique group URLs
-    unique_group_url_count = count_unique_group_urls(json_data)
-    print(f"Unique Group URL Count: {unique_group_url_count}")
-
-    # Initialize crawled data
+    starting_urls_data = load_json('Groups/Overall_data/data1_post.json')
     crawled_data = []
 
-    # Iterate through each starting URL
-    for item in json_data:
-        starting_url = item['group_url']
-        title = item['title']
+    for entry in starting_urls_data:
+        starting_url = entry["download_data"]
+        title = entry["title"]
 
-        # Renew Tor IP before starting for each URL
-        renew_tor_ip()
+        renew_tor_ip()  # Renew Tor IP before starting
+        discovered_websites = crawl_with_tor(starting_url)
 
-        # Crawl and get discovered websites
-        max_crawl_depth = 4
-        discovered_websites = crawl_with_tor(starting_url, max_depth=max_crawl_depth)
+        # Filter out websites that are already in data_post.json's "download_data"
+        discovered_websites = [site for site in discovered_websites if site != starting_url]
 
-        # Filter out discovered websites present in 'download_data'
-        filtered_discovered_websites = [website for website in discovered_websites
-                                        if website not in item.get('download_data', '')]
+        crawled_entry = {"group_url": starting_url, "title": title}
+        for i, website in enumerate(discovered_websites, start=1):
+            crawled_entry[f"Discovered website {i}"] = website
 
-        # Create a dictionary for the crawled JSON
-        crawled_entry = {
-            'group_url': starting_url,
-            'title': title,
-        }
-
-        # Add discovered websites to the dictionary
-        for i, website in enumerate(filtered_discovered_websites, start=1):
-            crawled_entry[f'Discovered website {i}'] = website
-
-        # Append the dictionary to the list
         crawled_data.append(crawled_entry)
 
     # Save the crawled data to crawled.json
-    save_to_crawled_json(crawled_data)
+    save_json('Groups/Overall_data/crawled.json', crawled_data)
 
-    print("Crawled data has been saved to crawled.json")
+    print("Crawling and saving completed.")
