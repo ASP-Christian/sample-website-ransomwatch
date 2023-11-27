9c47da31bc34aef6a7c3c88bf13e8a26fb6a30e7
ASP- Christian <128400825+ASP-Christian@users.noreply.github.com>
2023-11-28 07:08:12 +0800

    Update Random_url_generator.py

--- Groups/Random_url_generator.py
+++ Groups/Random_url_generator.py
@@ -22,24 +22,6 @@ tor_proxy = {
     'https': 'socks5h://localhost:9050',
 }
 
-def generate_code(base_code):
-    # Extract the desired part from the base code
-    extracted_part = base_code.split('//')[-1].split('.')[0]
-
-    # Generate a random code based on the extracted part
-    result = ""
-    for char in extracted_part:
-        if char.isalpha():
-            result += random.choice(string.ascii_letters)
-        elif char.isdigit():
-            result += random.choice(string.digits)
-        else:
-            result += char
-
-    # Add "http://" at the beginning and ".onion" at the end
-    generated_code = "http://" + result + ".onion"
-    return generated_code
-
 def check_active(link):
     try:
         renew_tor_ip()
@@ -49,29 +31,25 @@ def check_active(link):
 
         if 200 <= status_code < 300:
             return True  # Link is active
+        else:
+            print(f"Unexpected status code {status_code} for {link}")
+            print(response.text)  # Print response content for debugging
+            print(response.headers)  # Print response headers for debugging
+
     except requests.exceptions.RequestException as e:
         print(f"Error for {link}: {e}")
     except Exception as e:
         print(f"An unexpected error occurred for {link}: {str(e)}")
     return False  # Link is not active
 
-# Load base codes from a JSON file
-with open('Overall_data/small_sample.json') as json_file:
-    data = json.load(json_file)
-    base_codes = [group['group'] for group in data]
+# Test URLs
+test_urls = [
+    "http://5n4qdkw2wavc55peppyrelmb2rgsx7ohcb2tkxhub2gyfurxulfyd3id.onion/",
+    "http://12323332ub2gyfurxulfyd3id.onion/"
+]
 
-# Number of codes to generate
-num_codes = 5
-
-# Generate and store the codes
-generated_codes = []
-for _ in range(num_codes):
-    random_base_code = random.choice(base_codes)
-    generated_code = generate_code(random_base_code)
-    generated_codes.append(generated_code)
-
-# Check if the generated codes are active using Tor
-for code in generated_codes:
-    is_active = check_active(code)
+# Check status for each test URL
+for url in test_urls:
+    is_active = check_active(url)
     status = "Success" if is_active else "Not Active"
-    print(f"{code}: {status}")
+    print(f"{url}: {status}")
