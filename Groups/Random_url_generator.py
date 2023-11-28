0e184700484ec53a6c72daf691253e6c7f38285a
ASP- Christian <128400825+ASP-Christian@users.noreply.github.com>
2023-11-28 08:11:16 +0800

    Update Random_url_generator.py

--- Groups/Random_url_generator.py
+++ Groups/Random_url_generator.py
@@ -2,13 +2,9 @@ import random
 import string
 import json
 import requests
-import socks
-import socket
-from requests.exceptions import RequestException
+from bs4 import BeautifulSoup
 from stem import Signal
 from stem.control import Controller
-from bs4 import BeautifulSoup
-from datetime import datetime
 
 # Function to renew the TOR IP address
 def renew_tor_ip():
@@ -22,6 +18,24 @@ tor_proxy = {
     'https': 'socks5h://localhost:9050',
 }
 
+def generate_code(base_code):
+    # Extract the desired part from the base code
+    extracted_part = base_code.split('//')[-1].split('.')[0]
+
+    # Generate a random code based on the extracted part
+    result = ""
+    for char in extracted_part:
+        if char.isalpha():
+            result += random.choice(string.ascii_letters)
+        elif char.isdigit():
+            result += random.choice(string.digits)
+        else:
+            result += char
+
+    # Add "http://" at the beginning and ".onion" at the end
+    generated_code = "http://" + result + ".onion"
+    return generated_code
+
 def check_active(link):
     try:
         renew_tor_ip()
@@ -31,27 +45,35 @@ def check_active(link):
 
         if 200 <= status_code < 300:
             soup = BeautifulSoup(response.content, 'html.parser')
-            # Add your BeautifulSoup parsing logic here
 
-            # For demonstration, let's print the title of the page
-            title = soup.title.string.strip()
-            print(f"{link}: Status Code {status_code}, Title: {title}")
+            # Check for a specific element that indicates the page is active
+            if soup.find('div', class_='active-indicator'):
+                return True  # Link is active
 
-            return status_code  # Return status code if link is active
-
-    except RequestException as e:
+    except requests.exceptions.RequestException as e:
         print(f"Error for {link}: {e}")
     except Exception as e:
         print(f"An unexpected error occurred for {link}: {str(e)}")
     
-    return None  # Return None if link is not active or an error occurred
+    return False  # Link is not active
 
-# Test URLs
-test_urls = [
-    "http://5n4qdkw2wavc55peppyrelmb2rgsx7ohcb2tkxhub2gyfurxulfyd3id.onion/",
-    "https://ahmia.fi/"
-]
+# Load base codes from a JSON file
+with open('Overall_data/small_sample.json') as json_file:
+    data = json.load(json_file)
+    base_codes = [group['group'] for group in data]
 
-# Check status for each test URL
-for url in test_urls:
-    check_active(url)
+# Number of codes to generate
+num_codes = 10000
+
+# Generate and store the codes
+generated_codes = []
+for _ in range(num_codes):
+    random_base_code = random.choice(base_codes)
+    generated_code = generate_code(random_base_code)
+    generated_codes.append(generated_code)
+
+# Check if the generated codes are active using Tor
+for code in generated_codes:
+    is_active = check_active(code)
+    status = "Success" if is_active else "Not Active"
+    print(f"{code}: {status}")
