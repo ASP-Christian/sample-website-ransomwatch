0d94e673b8fb0590876021cd76aa34ab2eb40572
ASP- Christian <128400825+ASP-Christian@users.noreply.github.com>
2023-11-28 10:14:25 +0800

    Update Random_url_generator.py

--- Groups/Random_url_generator.py
+++ Groups/Random_url_generator.py
@@ -1,12 +1,10 @@
 import random
 import string
-import json
 import requests
 from stem import Signal
 from stem.control import Controller
-from requests.exceptions import ConnectionError
+from requests.exceptions import RequestException
 from bs4 import BeautifulSoup
-from datetime import datetime
 
 # Function to renew the TOR IP address
 def renew_tor_ip():
@@ -20,23 +18,20 @@ tor_proxy = {
     'https': 'socks5h://localhost:9050',
 }
 
-def generate_code(base_code):
-    # Extract the desired part from the base code
-    extracted_part = base_code.split('//')[-1].split('.')[0]
-
-    # Generate a random code based on the extracted part
-    result = ""
-    for char in extracted_part:
-        if char.isalpha():
-            result += random.choice(string.ascii_letters)
-        elif char.isdigit():
-            result += random.choice(string.digits)
-        else:
-            result += char
-
-    # Add "http://" at the beginning and ".onion" at the end
-    generated_code = "http://" + result + ".onion"
-    return generated_code
+# Your generated codes
+generated_codes = [
+    "https://ahmia.fi/",
+    "http://nq4zyac4ukl4tykmidbzgdlvaboqeqsemkp4t35bzvjeve6zm2lqcjid.onion",
+    "https://3f7nxkjway3d223j27lyad7v5cgmyaifesycvmwq7i7cbs23lb6llryd.onion",
+    "http://4ikm4jakjgmkezytyawtdgr2xymvy6nvzgw5cglswg3si76icnqd.onion",
+    "http://alphvmmm27o3abo3r2mlmjrpdmzle3rykajqc5xsj7j7ejksbpsa36ad.onion",
+    "http://jbeg2dct2zhku6c2vwnpxtm2psnjo2xnqvvpoiiwr5hxnc6wrp3uhnad.onion",
+    "http://bianlianlbc5an4kgnay3opdemgcryg2kpfcbgczopmm3dnbz3uaunad.onion",
+    "http://cuba4ikm4jakjgmkezytyawtdgr2xymvy6nvzgw5cglswg3si76icnqd.onion/",
+    "http://sbc2zv2qnz5vubwtx3aobfpkeao6l4igjegm3xx7tk5suqhjkp5jxtqd.onion/",
+    "http://7ukmkdtyxdkdivtjad57klqnd3kdsmq6tp45rrsxqnu76zzv3jvitlqd.onion/",
+    "http://5n4qdkw2wavc55peppyrelmb2rgsx7ohcb2tkxhub2gyfurxulfyd3id.onion/"
+]
 
 def check_active(link):
     try:
@@ -47,22 +42,14 @@ def check_active(link):
 
         if 200 <= status_code < 300:
             return True  # Link is active
-    except requests.exceptions.RequestException as e:
+    except RequestException as e:
         print(f"Error for {link}: {e}")
     except Exception as e:
         print(f"An unexpected error occurred for {link}: {str(e)}")
     return False  # Link is not active
 
-# Load base codes from a JSON file
-with open('Overall_data/small_sample.json') as json_file:
-    data = json.load(json_file)
-    base_codes = [group['group'] for group in data]
-
-# Continue generating codes until a success status is found
-success_found = False
-while not success_found:
-    generated_code = generate_code(random.choice(base_codes))
-    is_active = check_active(generated_code)
+# Check if the generated codes are active using Tor
+for code in generated_codes:
+    is_active = check_active(code)
     status = "Success" if is_active else "Not Active"
-    print(f"{generated_code}: {status}")
-    success_found = is_active
+    print(f"{code}: {status}")
